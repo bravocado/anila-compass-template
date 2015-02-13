cd7b67cd6d789e28585a0605ef001bf89c123263
Bravocado <bravocado.project@gmail.com>
2015-02-13 22:38:52 +0700

    compass config file sass_dir

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ add_import_path "bower_components/anila/sass"
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "dist/css"
-sass_dir = "build/scss"
+sass_dir = "build/sass"
 images_dir = "dist/img"
 javascripts_dir = "dist/js"
 
